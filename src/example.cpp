0428fefbf2dd6ecc7ccf95f03903d313c46e0e3c
Bendik Samseth <b.samseth@gmail.com>
2019-04-13 22:41:40 +0200

    Change example code to not emmit any warnings
    Clang warned about one of the branches never being reached. Simply use
    some other C++17 feature.

--- src/example.cpp
+++ src/example.cpp
@@ -5,9 +5,8 @@ Dummy::Dummy() {
 }
 
 bool Dummy::doSomething() {
-    // Do a silly test, but using if constexpr to enforce C++17 builds only.
-    if constexpr (1 + 1 > 1)
-        return true;
-    else
-        return false;
+    // Do silly things, using some C++17 features to enforce C++17 builds only.
+    constexpr int digits[2] = {0, 1};
+    auto [zero, one] = digits;
+    return zero + one;
 }
